608a423ed10f3237912530dbb7f6835842759346
mahesh <maheshmanoharan7251@gmail.com>
2023-02-03 23:12:16 +0530

    color resources file added with some colors

---
+++ lib/utils/color_resources.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorResources
+{
+    static const Color BACKGROUND_COLOR = Color(0xffFAFAFA);
+    static const Color TEXT_COLOR = Color(0xff373e3f);
+    static const Color RADIO_BUTTON_COLOR = Color(0xff7a2544);
+    static const Color TOP_BUTTON_COLOR = Color(0xfff5f5f5);
+    static const Color CARD_COLOR = Color(0xffffffff);
+    static const Color SNACKBAR_COLOR = Color(0xff465152);
+    static const Color TEXT_DEFAULT_COLOR = Color(0xff808080);
+}
